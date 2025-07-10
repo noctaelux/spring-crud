9681284f1d39098cb1a3a5d516d2ac69aa2d44f3
Oscar Calzada <noctaelux@gmail.com>
2025-07-09 20:22:32 -0600

    Add docker-compose for CRUD apps and rename variable in reactive API

--- src/main/java/com/example/springcrud/api/ReactiveApi.java
+++ src/main/java/com/example/springcrud/api/ReactiveApi.java
@@ -10,11 +10,11 @@ import java.time.Duration;
 @RequestMapping("/reactive")
 public class ReactiveApi {
 
-    @GetMapping(path = "/backpressure/{count}/{message}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> stream(@PathVariable("count") Long count, @PathVariable("message") String message) {
+    @GetMapping(path = "/backpressure/{interval}/{message}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> stream(@PathVariable("interval") Long interval, @PathVariable("message") String message) {
 
         return Flux
-                .interval(Duration.ofNanos(count))
+                .interval(Duration.ofNanos(interval))
                 .onBackpressureBuffer(1000)
                 .onBackpressureDrop()
                 .map(l -> String.format("Hello %s - sending message # %d.", message, l));
